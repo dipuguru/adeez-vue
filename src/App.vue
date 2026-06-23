93542ac13dd8b278da978e2ee21bd516dc3ad383
Dipali <dimpleg2188@gmail.com>
2026-06-23 17:59:40 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HeaderSection />
+  <HeroSection />
+</template>
+
+<script>
+import HeaderSection from './components/HeaderSection.vue'
+import HeroSection from './components/HeroSection.vue'
+
+export default {
+  components: {
+    HeaderSection,
+    HeroSection
+  }
+}
+</script>
